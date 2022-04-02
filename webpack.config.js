67af99f9c2384c5e92314d903980d2146df9a859
gkxiaoqi <gk.xiaoqi@gmail.com>
2022-04-02 11:08:33 +0800

    webpack配置调整

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // webpack5以上不能使用extract-text-webpack-plugin
-const path = require("path"); // nodejs里面的基本包，用来处理路径
-const { VueLoaderPlugin } = require("vue-loader");
-const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // webpack5以上不能使用extract-text-webpack-plugin
+const path = require('path'); // nodejs里面的基本包，用来处理路径
+const { VueLoaderPlugin } = require('vue-loader');
+const webpack = require('webpack');
 
 module.exports = (env = {}) => {
-  const mode = env.production ? "production" : "development";
+  const mode = env.production ? 'production' : 'development';
   return {
     // 入口文件
     /*
@@ -14,18 +14,18 @@ module.exports = (env = {}) => {
      * resolve()方法不会利用底层的文件系统判断路径是否存在，而只是进行路径字符串操作
      */
     mode,
-    entry: path.join(__dirname, "src/main.js"),
+    entry: path.join(__dirname, 'src/main.js'),
     // 打包配置
     output: {
-      filename: "bundle.js",
-      path: path.join(__dirname, "dist"),
+      filename: 'bundle.js',
+      path: path.join(__dirname, 'dist'),
     },
     resolve: {
       alias: {
-        "@": path.join(__dirname, "src"),
+        '@': path.join(__dirname, 'src'),
       },
       // 引入文件时不需要加后缀
-      extensions: [".js", ".vue"],
+      extensions: ['.js', '.vue'],
     },
     /**
      * plugin和loader的区别
@@ -35,13 +35,13 @@ module.exports = (env = {}) => {
     plugins: [
       new webpack.DefinePlugin({
         // 不使用JSON.stringfy的话mode的值会被识别成变量，比如mode='production'，会试图读取production变量，使用'"production"'这种写法也行
-        "process.env.NODE_ENV": JSON.stringify(mode),
+        'process.env.NODE_ENV': JSON.stringify(mode),
       }),
       new VueLoaderPlugin(),
       // 将打包后的文件插入到一个html文件中
       new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: "./public/index.html",
+        filename: 'index.html',
+        template: './public/index.html',
       }),
       new MiniCssExtractPlugin(),
     ],
@@ -50,34 +50,34 @@ module.exports = (env = {}) => {
         {
           test: /\.js$/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/env"], // 也可以写成presets: ['babel-preset-env']
-              plugins: ["@babel/plugin-transform-runtime"],
+              presets: ['@babel/env'], // 也可以写成presets: ['babel-preset-env']
+              plugins: [['@babel/plugin-transform-runtime', { corejs: false }]],
             },
           },
-          exclude: "/node_modules/",
+          exclude: '/node_modules/',
         },
         {
           test: /\.vue$/,
-          use: ["vue-loader"],
+          use: ['vue-loader'],
         },
         {
           test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.styl(us)?$/,
-          use: ["style-loader", "css-loader", "stylus-loader"],
+          use: ['style-loader', 'css-loader', 'stylus-loader'],
         },
       ],
     },
     // 输出source-map方便调试
-    devtool: "source-map",
+    devtool: 'source-map',
     // devServer的作用是开启一个开发用的服务器，用于快速开发，并会监听本地代码变动
     devServer: {
       // 设置成0.0.0.0可以同时监听localhost（即127.0.0.1）和本机局域网ip地址
-      host: "0.0.0.0",
+      host: '0.0.0.0',
       // webpack4之前
       client: {
         overlay: {
